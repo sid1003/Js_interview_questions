fd020a7144d52bc96a7aff04a9787c5de8c2d981
sidmeshram1003 <sidmeshram1003@gmail.com>
2025-08-12 07:34:24 +0530

    minor update

--- 16_polyfills/Functions/apply.js
+++ 16_polyfills/Functions/apply.js
@@ -1,17 +1,19 @@
-Function.prototype.myApply = function(context = {}, args = []) {
-    if (typeof this !== 'function') {
-        throw new TypeError(`${this} is not a function`);
-    }
+Function.prototype.myApply = function (context = {}, args = []) {
+  if (typeof this !== "function") {
+    throw new TypeError(`${this} is not a function`);
+  }
 
-    context = context ?? globalThis;
-    const fnSymbol = Symbol();
-    context[fnSymbol] = this;
+  if (!Array.isArray(args) && args !== null && args !== undefined) {
+    throw new TypeError(
+      "The second argument must be an array, null, or undefined"
+    );
+  }
 
-    if (!Array.isArray(args) && args !== null && args !== undefined) {
-        throw new TypeError("The second argument must be an array, null, or undefined");
-    }
+  context = context ?? globalThis;
+  const fnSymbol = Symbol();
+  context[fnSymbol] = this;
 
-    const result = args ? context[fnSymbol](...args) : context[fnSymbol]();
-    delete context[fnSymbol];
-    return result;
+  const result = args ? context[fnSymbol](...args) : context[fnSymbol]();
+  delete context[fnSymbol];
+  return result;
 };
